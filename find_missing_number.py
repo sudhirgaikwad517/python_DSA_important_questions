c3aa392096bab30ead8133ece72ff9453d6d75e1
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-07-26 22:34:52 +0530

    Initial commit

---
+++ find_missing_number.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 26 22:20:49 2025
+
+@author: Sudhir Gaikwad
+"""
+
+# find missing number
+
+def find_missing_number(arr, n):
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(arr)
+    return expected_sum - actual_sum
+
+# Example:
+print(find_missing_number([1, 2, 4, 5, 6], 6))  # Output: 3
